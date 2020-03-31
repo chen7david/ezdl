35ded9120b34d6437b28f268218c40cb2fa89afe
David Chen <chen7david@me.com>
2020-03-31 11:36:25 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# ezdl
+# EZ-Downlaoder
+
+```js
+const DL = require('ezdl')({
+    folder: path.resolve(__dirname, 'downloads'),
+    baseURL:'http://some-url/'
+})
+
+const configured = async () => {
+
+       const result = await DL
+            .params('url-param-one', 'url-param-two', /* url-param-n */)
+            .name('file-name')
+            .start('parent-folder', 'child-foler-one', /* child-foler-n */)
+
+}
+
+configured()
+```
+
+```js
+const { Downloader } = require('ezdl')
+const DL = new Downloader()
+
+const instance = async () => {
+
+    const result = await DL
+        .baseURL('http://image.tmdb.org/t/p/')
+        .params('url-param-one', 'url-param-two', /* url-param-n */)
+        .name('file-name')
+        .start('parent-folder', 'child-foler-one', /* child-foler-n */)
+
+}
+
+instance()
+```
+
+
+```js
+.force(/* boolean */) /* optional - foreces redownload of already existing files */ 
+```
---
+++ index.js
@@ -0,0 +1,82 @@
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const dd = (val) => console.log(val)
+
+class Downloader {
+
+    constructor(options = {}){
+        const {folder, baseURL} = options
+        this.folder = folder ? folder : __dirname + '/../../'
+        this._baseURL = baseURL ? baseURL : ''
+        this._params = ''
+        this._force = false
+        this._name = null
+    }
+
+    baseURL(url){
+        this._baseURL = url
+        return this
+    }
+
+    params(...params){
+        this._params = path.join(...params)
+        return this
+    }
+
+    url(){
+        return this._baseURL + this._params
+    }
+
+    force(bool){
+        this._force = bool ? bool : false
+        return this
+    }
+
+    async exists(filePath){
+        try {
+            await fs.promises.access(filePath)
+            return true
+        } catch (err) {
+            return false
+        }
+    }
+
+    name(name){
+        this._name = name
+        return this
+    }
+
+    async start(subfolders = []){
+        try {
+            if(!this._name) throw('please set file name')
+            const dir = path.join(this.folder, ...subfolders)
+            const filePath = path.join(dir, this._name)
+
+            await fs.promises.mkdir(dir, {recursive: true})
+
+            if(!await this.exists(filePath) || this._force){
+
+                const { data } = await axios({
+                    url: this.url(),
+                    responseType: 'stream',
+                })
+
+                data.pipe(fs.createWriteStream(filePath))
+
+                return new Promise((resolve, reject) => {
+                    data.on('end', () => { resolve(true) })
+                    data.on('error', () => { reject(err)})
+                })
+            }
+            return true
+        } catch (err) {
+            dd(err)
+        }
+    }
+
+}
+
+exports = module.exports = (options = {}) => new Downloader(options)
+
+exports.Downloader = Downloader
